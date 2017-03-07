9e39bffc8f47080fdacf5114fd2dcf2f32cc4d81
johangw <johangandawijaya@gmail.com>
2017-03-07 22:00:15 +0700

    Add files via upload

---
+++ daftar_buku.php
@@ -0,0 +1,10 @@
+<?php
+  $filename = "buku_tamu.txt";
+  $file = fopen($filename,"r");
+  echo "<h2>Daftar Buku Tamu</h2>";
+  $handle = fopen($filename, "r");
+  while ($record = fgetcsv($handle,0)) {
+    echo "Nama: $record[0], Email: $record[1], Pesan: $record[2] <br/>";
+  }
+  fclose ($handle);
+?>
