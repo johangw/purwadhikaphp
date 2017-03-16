636455207cf5f6e518d0566a46834867fdefa0e7
johangw <johangandawijaya@gmail.com>
2017-03-16 21:17:24 +0700

    Update Daftar_Pegawai.php

--- Pegawai/Daftar_Pegawai.php
+++ Pegawai/Daftar_Pegawai.php
@@ -2,14 +2,13 @@
 	include_once("file.class.php");
 	include_once("Table.class.php");
 	
+	$datarec = array();
 	$namafile ="D:\\xampp\\htdocs\purwadhika\\pegawai.txt";
 	// initiate object (create object)
 	$file = new File($namafile,"");
 	// echo "<h2>Daftar Pegawai</h2>";
 	// echo "<table border=1>";
 	$tblHeader = array("NIP","Nama","Email","Waktu Simpan");
-	$tPegawai = new Table("Daftar Pegawai",$tblHeader,"");
-	$tPegawai->generateTable();
 	// membaca method ambilFormatCSV
 	/*
 	while ($record = $file->ambilFormatCSV()) {
@@ -17,5 +16,19 @@
 	}
 	echo "</table>";
 	*/
+	//
+	$idx=0;
+	while ($record = $file->ambilFormatCSV()) {
+		for ($x=0;$x<count($tblHeader);$x++) {
+			$datarec[$idx][$tblHeader[$x]] = $record[$x];
+		}
+		$idx++;
+	}
+	$tPegawai = new Table("Daftar Pegawai", $tblHeader, $datarec);
+	$tPegawai->generateTable();	
+	$html = $tPegawai->generateToHTML();
+	$file->menutupFile();
+	$namafile ="D:\\xampp\\htdocs\purwadhika\\daftarpegawai.html";
+	$file = new File($namafile,$html);
 	$file->menutupFile();
 ?>
